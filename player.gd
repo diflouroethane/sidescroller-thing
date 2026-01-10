b01f275c676f8ed2ac5b51e95b1a2c91bb09f45b
Ethan Francis <ethanfrancis577@gmail.com>
2026-01-09 19:29:52 -0500

    initial commit

---
+++ player.gd
@@ -0,0 +1,92 @@
+extends CharacterBody2D
+
+class_name Player
+
+const WALKING_SPEED: float = 15_000.0
+const ROLLING_SPEED: float = 30_000.0
+
+#const SLIDING_SPEED: float = 30_000.0
+#var sliding: bool = false
+var rolling: bool = false
+var invuln: bool = false
+var can_attack: bool = true
+
+var direction: float
+
+var dirs: Dictionary = {"UP": "Up", "LEFT": "Left", "RIGHT": "Right"}
+var loc:  String = "Up"
+
+enum states {
+	IDLE,
+	RUN,
+	#SLIDE,
+	ROLL,
+	SPRINT,
+	ATTACK
+}
+var state: states = states.IDLE
+
+@onready var slash: PackedScene = load("uid://b70go1gmhi1sw")
+
+#func is_sliding()->bool:
+	#return (state == states.SLIDE)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	#### NO JUMPING >:(
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+	
+	state_logic(delta)
+	
+	if Input.is_action_just_pressed("left"):
+		loc = dirs["LEFT"]
+	if Input.is_action_just_pressed("right"):
+		loc = dirs["RIGHT"]
+	if Input.is_action_just_pressed("up"):
+		loc = dirs["UP"]
+	
+	if Input.is_action_just_pressed("attack") and can_attack:
+		var a: Slash = slash.instantiate()
+		if loc == dirs["LEFT"]:
+			a.get_node("Texture").flip_h = true
+		a.get_node("Texture").connect("animation_finished", func (): can_attack = true)
+		a.global_transform = $locations.get_node(loc).transform
+		can_attack = false
+		add_child(a)
+	
+	direction = Input.get_axis("left", "right") if state != states.ROLL else direction
+	
+	$Animation.flip_h = true if direction < 0 else false
+	
+	if direction:
+		var spd = WALKING_SPEED
+		velocity.x = direction * spd * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, WALKING_SPEED)
+	
+	if Input.is_action_pressed("roll") && direction != 0:
+		state = states.ROLL
+	
+	move_and_slide()
+
+func state_logic(_delta: float) -> void:
+	if state == states.ROLL:
+		$Animation.play("rolling")
+		pass
+		#$walking.disabled = true
+		#$sliding.disabled = false
+	else:
+		$Animation.play("walking")
+		#$sliding.disabled = true
+		$walking.disabled = false
+
+func allow_slash() -> void:
+	can_attack = true
+
+
+func _on_animation_finished() -> void:
+	state = states.IDLE
---
+++ slash.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+class_name Slash
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Texture.play("slash")
+	
+
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
